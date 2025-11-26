5679546633f414df1be39cf578ff6749708d7b66
moha528 <mtseckdev@gmail.com>
2025-11-26 21:14:40 +0000

    Feature: Ajout de la localisation en langue Wolof

--- P2FixAnAppDotNetCode/Startup.cs
+++ P2FixAnAppDotNetCode/Startup.cs
@@ -41,7 +41,7 @@ namespace P2FixAnAppDotNetCode
                 .AddDataAnnotationsLocalization();
 
             services.Configure<RequestLocalizationOptions>(opts =>
-            { 
+            {
                 var supportedCultures = new List<CultureInfo>
                 {
                     new CultureInfo("en-GB"),
@@ -49,6 +49,7 @@ namespace P2FixAnAppDotNetCode
                     new CultureInfo("en"),
                     new CultureInfo("fr-FR"),
                     new CultureInfo("fr"),
+                    new CultureInfo("wo"),
                 };
 
                 opts.DefaultRequestCulture = new RequestCulture("en");
